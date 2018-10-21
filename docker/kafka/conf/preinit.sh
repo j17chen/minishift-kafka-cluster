bb9e8c8698125526cc4607208c6373c2e34a7051
jchen <jchen@hortonworks.com>
2018-10-20 23:46:11 -0700

    update cluster to enable scalable count for kafka and zookeeper

--- docker/kafka/conf/preinit.sh
+++ docker/kafka/conf/preinit.sh
@@ -1,3 +1,17 @@
+
+KAFKA_SERVER_CFG="/etc/kafka/conf/server.properties"
+if [ $(cat $KAFKA_SERVER_CFG | grep "zookeeper.connect=MY_ZOOKEEPER_QUORUM"| wc -l) == 1 ];then
+	echo "Initialize quorum settings...";
+	MY_ZK_QUORUM="${ZOOKEEPER_INSTANCE_NAME}-0.${ZOOKEEPER_INSTANCE_NAME}.${MY_POD_NAMESPACE}.${MY_DOMAIN_NAME}:2181";
+	for i in $(seq 2 1 ${ZOOKEEPER_INSTANCE_COUNT});do
+		HOSTID=$[ $i - 1];
+		SERVER_DATA="${ZOOKEEPER_INSTANCE_NAME}-${HOSTID}.${ZOOKEEPER_INSTANCE_NAME}.${MY_POD_NAMESPACE}.${MY_DOMAIN_NAME}:2181"
+		MY_ZK_QUORUM=${MY_ZK_QUORUM},${SERVER_DATA}
+	done
+	echo "ZK quorum : ${MY_ZK_QUORUM}"
+	sed -i "s/zookeeper.connect=MY_ZOOKEEPER_QUORUM/zookeeper.connect=${MY_ZK_QUORUM}/g" $KAFKA_SERVER_CFG
+fi 
+
 ZK_QUORUM=$(cat /etc/kafka/conf/server.properties | grep "zookeeper.connect=" | awk -F = '{print $2}')
 echo "Waitting ZK_QUORUM $ZK_QUORUM ready..."
 until /usr/hdf/current/zookeeper-client/bin/zkCli.sh -server $ZK_QUORUM ls / &> /dev/null;do echo -n ".";sleep 1;done;
--- docker/zookeeper/conf/preinit.sh
+++ docker/zookeeper/conf/preinit.sh
@@ -1,3 +1,16 @@
+ZOO_CFG="/etc/zookeeper/conf/zoo.cfg"
+
+if [ $(grep "server." $ZOO_CFG | wc -l ) = 0 ];then
+	echo "Initialize quorum settings...";
+	echo >> $ZOO_CFG
+	for i in $(seq 1 1 ${ZOOKEEPER_INSTANCE_COUNT});do
+		HOSTID=$[ $i - 1];
+		SERVER_DATA="server.${i}=${ZOOKEEPER_INSTANCE_NAME}-${HOSTID}.${ZOOKEEPER_INSTANCE_NAME}.${MY_POD_NAMESPACE}.${MY_DOMAIN_NAME}:2888:3888"
+		echo $SERVER_DATA
+		echo $SERVER_DATA >> $ZOO_CFG
+	done
+fi
+
 if [ ! -f "/zookeeper/myid" ];then
 	echo "Quorum id doesn't exist, generate Quorum id..."
 	RAWID=`echo $(hostname) | awk -F - '{print $2}' `
@@ -8,5 +21,5 @@ if [ ! -f "/zookeeper/myid" ];then
 fi
 echo "Waitting all zookeeper nodes ready..."
 sleep 10
-cat /etc/zookeeper/conf/zoo.cfg | grep server. | awk -F = '{print $2}'| awk -F : '{print $1}'| while read h; do until nslookup $h ;do echo "wait $h ready...";sleep 10;done;echo "ZK host $h Is ready";done
+cat $ZOO_CFG | grep server. | awk -F = '{print $2}'| awk -F : '{print $1}'| while read h; do until nslookup $h ;do echo "wait $h ready...";sleep 10;done;echo "ZK host $h Is ready";done
 echo "All ZK Nodes is ready"
